d1aa7e7ac5d8aef9334d32a03d5e51b1cf13ac39
Christian G. Warden <cwarden@xerus.org>
2017-10-23 16:50:37 -0700

    Fix LastUpdate When NPI_Update Is Empty
    Do not return an error when default value is returned from LastUpdate.
    
    Add tests for LastUpdate.

---
+++ suite_test.go
@@ -0,0 +1,13 @@
+package npidb_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCommand(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Library Suite")
+}
